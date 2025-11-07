cbb259799d02a2b9b75f12f5ed16b6709ed3bcb9
Basil Varghese <basilvarghese4566@gmail.com>
2025-11-07 12:34:26 +0530

    laying down foundation

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement(
-            "div",
-            {id:"child"},
-            [
-               React.createElement("h1",{},"im an h1 tag"),
-               React.createElement("h2",{},"im an h2 tag")
-            ]
-        ),
-        React.createElement(
-            "div",
-            {id:"child2"},
-            [
-                React.createElement("h1",{},"im an h1 tag"),
-                React.createElement("h2",{},"im an h2 tag")
-            ]
-        )
-    ]
+const elem = <span>React Element</span>
+
+// react element
+const jsxHeading = (
+    <h1 id="heading">
+        {elem}
+        hello world
+    </h1>
+);
+
+
+//# component composition
+
+// 1st component
+const Title = ()=>(
+    
+    <h1 className="head" tabIndex="5">
+        namaste react functional component2
+    </h1>
 )
 
+// 2nd react functional component
+
+const number  = 1000;
+const HeadingComponet = () =>(
+    <div id="container">
+        {jsxHeading}
+        <h2>{number}</h2>
+        <Title />
+        <h1 className="heading">react functional component</h1>
+    </div>
+)
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
 
-
-
-
-
-
-
+root.render(<HeadingComponet/>)
